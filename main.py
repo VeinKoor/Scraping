ac9be9147f007b890f7ba23b427a9f1531a46230
Sergey <onetap.cs@yandex.ru>
2024-11-28 23:32:16 +0700

    optimise code

--- main.py
+++ main.py
@@ -6,25 +6,24 @@ from fake_useragent import UserAgent
 HEADERS = {'User-Agent': UserAgent().random}
 HOST = 'https://scrapingclub.com'
 
-list_card_url = []
+def get_url():
+    for page in range(1, 7):
+        URL = f'https://scrapingclub.com/exercise/list_basic/?page={page}'
+
+        response = requests.get(URL, headers=HEADERS)
+        soup = BeautifulSoup(response.text, 'lxml')
+        cards = soup.find_all('div', class_ = 'w-full rounded border')
+
+        for card in cards:
+            card_url = HOST + card.find('a').get('href')
+            yield card_url
 
 
-for page in range(1, 7):
-    URL = f'https://scrapingclub.com/exercise/list_basic/?page={page}'
-
-    response = requests.get(URL, headers=HEADERS)
-    soup = BeautifulSoup(response.text, 'lxml')
-    cards = soup.find_all('div', class_ = 'w-full rounded border')
-
-    for card in cards:
-        card_url = HOST + card.find('a').get('href')
-        list_card_url.append(card_url)
-
-    sleep(1)
 
 
-for card_url in list_card_url:
+for card_url in get_url():
     response = requests.get(card_url, headers=HEADERS)
+    sleep(3)
     soup = BeautifulSoup(response.text, 'lxml')
     card = soup.find('div', class_ = 'my-8 w-full rounded border')
 
